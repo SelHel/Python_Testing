396814b4f43f8bc44241f760779822625377c05a
selhel <helaoui.selim@gmail.com>
2022-06-07 18:31:28 +0200

    Fix Issue #1

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,3 +8,25 @@ def client():
     server.app.config['TESTING'] = True
     client = server.app.test_client()
     return client
+
+
+@pytest.fixture
+def mock_clubs(mocker):
+    clubs = [
+        {
+            "name": "Test Club 1",
+            "email": "club1@test.com",
+            "points": "13"
+        },
+        {
+            "name": "Test Club 2",
+            "email": "club2@test.com",
+            "points": "4"
+        },
+        {
+            "name": "Test Club 3",
+            "email": "club3@test.com",
+            "points": "12"
+        },
+    ]
+    mocker.patch.object(server, 'clubs', clubs)
--- tests/tests_unitaires/test_server.py
+++ tests/tests_unitaires/test_server.py
@@ -1,3 +1,13 @@
+def test_should_access_home(client):
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_showSummary_with_known_email(client, mock_clubs):
+    known_email = 'club3@test.com'
+    response = client.post('/showSummary', data={'email': known_email})
+    assert response.status_code == 200
+
 
 def test_showSummary_with_unknown_email(client):
     unknown_email = 'wrong@email.co'
@@ -5,18 +15,12 @@ def test_showSummary_with_unknown_email(client):
     assert response.status_code == 302
 
 
-def test_showSummary_with_known_email(client):
-    known_email = 'john@simplylift.co'
-    response = client.post('/showSummary', data={'email': known_email})
-    assert response.status_code == 200
+def test_showSummary_with_empty_email(client):
+    empty_email = ''
+    response = client.post('/showSummary', data={'email': empty_email})
+    assert response.status_code == 302
 
 
-def test_club_should_not_be_able_to_use_more_than_their_points_allowed():
-    pass
-
-
-def test_points_should_be_deducted_from_the_clubs_total():
-    pass
 
 
 
