f52f7158b7a2a785a72f8107eb46791bc7239c2f
selhel <helaoui.selim@gmail.com>
2022-06-09 16:04:42 +0200

    Update Tests Issue #2

--- tests/tests_unitaires/test_server.py
+++ tests/tests_unitaires/test_server.py
@@ -1,3 +1,5 @@
+from server import POINTS_FOR_A_PLACE
+
 """
 Bug :
 Quand un secrétaire échange plus de points qu'il en a à sa disposition,
@@ -27,3 +29,14 @@ def test_purchasePlaces_more_points_than_available(client, mock_clubs, mock_comp
     response = client.post('/purchasePlaces', data=invalid_data)
     assert response.status_code == 200
     assert "You cannot redeem more points than available!" in response.data.decode()
+
+
+def test_purchasePlaces_update_club_points(client, mock_clubs, mock_competitions):
+    valid_data = {'club': 'Test Club 1',
+                  'competition': 'Test Competition 1',
+                  'places': 12
+                  }
+    expected_club_value = 13 - (12 * POINTS_FOR_A_PLACE)
+    response = client.post('/purchasePlaces', data=valid_data)
+    assert response.status_code == 200
+    assert "Great-booking complete!" in response.data.decode()
